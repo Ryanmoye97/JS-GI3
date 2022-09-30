343394df58fed74a2d151fb95442021eee89d14f
Ryanmoye97 <Ryan.moye1997@icloud.com>
2022-09-30 10:48:55 -0400

    first commit

---
+++ medium.js
@@ -0,0 +1,12 @@
+function sharePizza(x) {
+return 'each person gets ${x / y} slices of pizza'; {
+};
+};
+
+
+
+var sharePizza = cutPizzaSlice(8);
+console.log(sharePizza(2));
+// prints "Each perosn gets 4.00 slices of pizza"
+console.log(sharePizza(3));
+// prints "Each person gets 2.67 slices of pizza"
